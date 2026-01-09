4eb12439341b09d4e9f44c7c298e75551ece96f3
Lizbeth Sanchez <lizbeth59312@gmail.com>
2026-01-08 22:43:32 -0500

    Primer commit, struct del producto

---
+++ struct_producto.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Lo que hago aqui es definir el cascaron de como va ser 
+
+using namespace std;
+
+struct Producto {
+    int id;
+    string nombre; //Este vendri a ser como el modelo 
+    string categoria;//Este el daño 
+    double precio;
+};
+
