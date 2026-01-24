20245c420993345e842a53777d70768f9a227043
Lizbeth Sanchez <lizbeth59312@gmail.com>
2026-01-24 16:38:19 -0500

    AcualizarDialog.cpp

---
+++ Proyecto_productos/AcualizarDialog.cpp
@@ -0,0 +1,14 @@
+#include "AcualizarDialog.h"
+#include "ui_AcualizarDialog.h"
+
+Dialog::Dialog(QWidget *parent)
+    : QDialog(parent)
+    , ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
